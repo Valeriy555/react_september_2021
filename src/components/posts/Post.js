0edec0221fe8d1f57318271f6db375dac43fbc5c
Valery555 <sokolchenkov@gmail.com>
2022-01-12 15:30:16 +0200

    12/01

--- src/components/posts/Post.js
+++
@@ -1,9 +0,0 @@
-export default function Post (props){
-    let {userId,id,title, body} = props;
-    return (
-        <div>
-            {userId}--{id}--{title}--{body}
-
-        </div>
-    );
-}
