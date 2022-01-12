d3c6d6267750884a5d4cda3257ef7e01b3a05bdf
Valery555 <sokolchenkov@gmail.com>
2022-01-12 23:14:33 +0200

    12/01

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import Users from "./components/users/Users"
-import Posts from "./components/posts/Posts"
-import Comments from "./components/comments/Comments"
+import React, {useEffect, useState} from 'react';
+
 
 const App = () => {
+    let [flightList,setFlightsList] = useState([])
+    useEffect(() => {
+        fetch('https://api.spacexdata.com/v3/launches')
+            .then(value => value.json())
+            .then(flights => {
+              setFlightsList (flights.filter(flight => flight.launch_year !== '2020'))
+
+            });
+    },[])
     return (
         <div>
-            <Users/>
-            <Posts/>
-            <Comments/>
+            {
+                flightList.map(value => <div key={value.flight_number}>
+                    {value.mission_name} - {value.launch_year}
+                   <img src={value.links.mission_patch} alt="mission_patch"/>
+                </div>)
+            }
+
         </div>
     );
 };
