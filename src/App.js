564b1b4c27e3d092ab235e6d0be2c378c5fd3c3f
Valery555 <sokolchenkov@gmail.com>
2022-01-22 16:19:34 +0200

    index

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import Users from "./components/users/Users"
-import Posts from "./components/posts/Posts"
-import Comments from "./components/comments/Comments"
+
+
 
 const App = () => {
     return (
         <div>
-            <Users/>
-            <Posts/>
-            <Comments/>
+
         </div>
     );
 };
