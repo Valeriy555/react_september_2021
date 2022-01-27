6c73cffaf73fad2851413228d07c0fd52ed93c2a
Valery555 <sokolchenkov@gmail.com>
2022-01-27 21:54:09 +0200

    27/01

--- src/App.js
+++ src/App.js
@@ -13,19 +13,19 @@ export default function App() {
         <div>
             <h2 className={css.box}>Homework</h2>
 
-            <h1> state1-{count_1}</h1>
+            <h1> state1: {count_1}</h1>
             <button onClick={() => dispatch({type: 'plus_10'})}>plus 10</button>
             <button onClick={() => dispatch({type: 'minus_3'})}>minus 3</button>
             <button onClick={() => dispatch({type: 'RESET_1', payload: 0})}>RESET</button>
             <hr/>
 
-            <h1> state2-{count_2}</h1>
+            <h1> state2: {count_2}</h1>
             <button onClick={() => dispatch({type: 'plus_2'})}>plus 2</button>
             <button onClick={() => dispatch({type: 'minus_2'})}>minus 2</button>
             <button onClick={() => dispatch({type: 'RESET_2', payload: 0})}>RESET</button>
             <hr/>
 
-            <h1> state3-{count_3}</h1>
+            <h1> state3: {count_3}</h1>
             <button onClick={() => dispatch({type: 'plus_1'})}>plus 1</button>
             <button onClick={() => dispatch({type: 'minus_1'})}>minus 1</button>
             <button onClick={() => dispatch({type: 'RESET_3', payload: 0})}>RESET</button>
