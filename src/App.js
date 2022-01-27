bb849330ae66780887d0959bc5c9792cba5e7ded
Valery555 <sokolchenkov@gmail.com>
2022-01-27 21:50:48 +0200

    27/01

--- src/App.js
+++ src/App.js
@@ -2,28 +2,34 @@ import {useReducer} from "react";
 
 
 import css from "./app.module.css";
+import reducer from "./Redusers/Redusers";
 
 export default function App() {
 
-    let [state,dispatch] = useReducer(reducer,{h:0, a:0, b:0, c:0});
-    let { a, b, c} = state;
+    let [state, dispatch] = useReducer(reducer, {count_1: 0, count_2: 0, count_3: 0});
+    let {count_1, count_2, count_3} = state;
     console.log(state)
     return (
         <div>
-            <h2 className={css.app.module} >Homework</h2>
+            <h2 className={css.box}>Homework</h2>
 
-            <h1> state1-{a}</h1>
-            <button onClick={() => dispatch({obj: '1', diya: '+'})}>plus 10</button>
-            <button onClick={() => dispatch({obj: '1', diya: '-'})}>minus 2 </button>
+            <h1> state1-{count_1}</h1>
+            <button onClick={() => dispatch({type: 'plus_10'})}>plus 10</button>
+            <button onClick={() => dispatch({type: 'minus_3'})}>minus 3</button>
+            <button onClick={() => dispatch({type: 'RESET_1', payload: 0})}>RESET</button>
             <hr/>
 
-            <h1> state2-{b}</h1>
-            <button onClick={() => dispatch({obj: '2', diya: '+'})}>plus 2</button>
-            <button onClick={() => dispatch({obj: '2', diya: '-'})}>minus 2</button>
+            <h1> state2-{count_2}</h1>
+            <button onClick={() => dispatch({type: 'plus_2'})}>plus 2</button>
+            <button onClick={() => dispatch({type: 'minus_2'})}>minus 2</button>
+            <button onClick={() => dispatch({type: 'RESET_2', payload: 0})}>RESET</button>
             <hr/>
-            <h1> state3-{c}</h1>
-            <button onClick={() => dispatch({obj: '3', diya: '+'})}>plus 5</button>
-            <button onClick={() => dispatch({obj: '3', diya: '-'})}>minus 1</button>
+
+            <h1> state3-{count_3}</h1>
+            <button onClick={() => dispatch({type: 'plus_1'})}>plus 1</button>
+            <button onClick={() => dispatch({type: 'minus_1'})}>minus 1</button>
+            <button onClick={() => dispatch({type: 'RESET_3', payload: 0})}>RESET</button>
+            <hr/>
 
         </div>
     );
---
+++ src/Redusers/Redusers.js
@@ -0,0 +1,41 @@
+const reducer = (state , action) => {
+
+    switch (action.type) {
+        case 'plus_10':
+            return {...state, count_1: state.count_1 + 10};
+        case 'minus_3':
+            return {...state, count_1: state.count_1 - 3};
+        case 'RESET_1':
+            return {...state, count_1: action.payload};
+
+        case 'plus_2':
+            return {...state, count_2: state.count_2 + 2};
+        case 'minus_2':
+            return {...state, count_2: state.count_2 - 2};
+        case 'RESET_2':
+            return {...state, count_2: action.payload};
+
+        case 'plus_1':
+            return {...state, count_3: state.count_3 + 1};
+        case 'minus_1':
+            return {...state, count_3: state.count_3 - 1};
+        case 'RESET_3':
+            return {...state, count_3: action.payload};
+
+
+        default:
+            return state
+    }
+
+}
+export default reducer;
+
+
+
+
+
+
+
+
+
+
