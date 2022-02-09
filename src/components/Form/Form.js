b2b4b17a3d7a0dbae75b56303d536be52e9bb942
Valery555 <sokolchenkov@gmail.com>
2022-02-09 02:25:19 +0200

    validation

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,30 +1,29 @@
 import {useForm} from "react-hook-form";
 import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from "react-redux";
+import {joiResolver} from "@hookform/resolvers/joi";
 
 import {createCar, updateCarById} from "../../store";
+import {CarValidator} from "../../validators/car.validator";
 
 const Form = () => {
     const {carForUpdate} = useSelector(genStore => genStore['carReducerSlice'])
 
-    const {handleSubmit, register, reset, setValue} = useForm(); // хук для работы с формами
+    const {
+        handleSubmit, register, reset, setValue, formState: {errors}
+    } = useForm({resolver: joiResolver(CarValidator), mode:"onTouched"}); // хук для работы с формами
 
     const dispatch = useDispatch();
 
-    const submitCreate = (data) => {
-        console.log(data);
-        dispatch(createCar({data}))
-         reset()
+    const submit = (data) => {
+        if (carForUpdate === null) {
+            dispatch(createCar({data}));
+            reset()
+        } else {
+            dispatch(updateCarById({id: carForUpdate.id, car: data}))
+            reset();
+        }
     }
-
-    const submitUpdate = (data) => {
-        console.log(data);
-        dispatch(updateCarById({id: carForUpdate.id, car: data}))
-        reset()
-    }
-
-
-
 
     useEffect(() => {
         if (carForUpdate) {
@@ -36,20 +35,22 @@ const Form = () => {
     }, [carForUpdate])
 
     return (
-        // <form onSubmit={handleSubmit(submit)}>
-        <form >
-
-            <label>Model: <input type="text" {...register('model')} /> </label>
-            <label>Price: <input type="text" {...register('price')}/> </label>
-            <label>Year: <input type="text" {...register('year')}/> </label>
-
-            <button onClick={handleSubmit(submitCreate)} >Save</button>
-            <button onClick={handleSubmit(submitUpdate)}>Update</button>
 
 
+        <form onSubmit={handleSubmit(submit)}>
+            <label/>Model:<input type='text' placeholder={'model'} {...register('model')}/>
+            {errors.model && <span>{errors.model.message}</span>}
 
+            <label/>Price:<input type='text' placeholder={'price'} {...register('price')}/>
+            {errors.price && <span>{errors.price.message}</span>}
 
+            <label/>Year:<input type='text' placeholder={'year'} {...register('year')}/>
+            {errors.year && <span>{errors.year.message}</span>}
+
+            <button>Save/Update</button>
         </form>
+
+
     );
 };
 export {Form};
